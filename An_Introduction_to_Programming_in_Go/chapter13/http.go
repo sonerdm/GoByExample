aa26d37eaa02992ace9b89557c6c79a46511d4a0
Soner <soner@Soners-MacBook-Pro.local>
2021-09-18 18:53:36 +0300

    practical go

--- An_Introduction_to_Programming_in_Go/chapter13/http.go
+++ An_Introduction_to_Programming_in_Go/chapter13/http.go
@@ -14,21 +14,21 @@ func hello(res http.ResponseWriter, req *http.Request) {
 		res,
 		`<DOCTYPE html>
 	<html>
-	<head>
-		<title>Hello World</title>
-	</head>
-	<body>
-		Hello World!
-	</body>
+		<head>
+			<title>Hello Bro</title>
+		</head>
+		<body>
+			Hello World!, Today is a beautiful day...
+		</body>
 	</html>`,
 	)
 }
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.Handle("/assets/",
-		http.StripPrefix(
-			"/assets/",
-			http.FileServer(http.Dir("assets")),
-		))
+	http.HandleFunc("/", hello)
+	// http.Handle("/assets/",
+	// 	http.StripPrefix(
+	// 		"/assets/",
+	// 		http.FileServer(http.Dir("assets")),
+	// 	))
 	http.ListenAndServe(":9000", nil)
 }
